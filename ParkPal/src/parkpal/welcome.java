e253cb3c15f2cef104983cfbaa3b2c770857c9e4
Trixia Danica Gregorio <trixiaagregorio@gmail.com>
2023-04-08 17:37:07 +0800

    Removed excess window. No more bugs or errors in homepage

--- ParkPal/src/parkpal/welcome.java
+++ ParkPal/src/parkpal/welcome.java
@@ -246,14 +246,6 @@ public class welcome extends javax.swing.JFrame {
         /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
          * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
          */
-        
-        JFrame welcome = new JFrame();
-        JPanel HomePagePanel = new JPanel();
-        welcome.setVisible(true);
-        welcome.add(HomePagePanel);
-        HomePagePanel.setVisible(true);
-        
-
         try {
             for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
                 if ("Nimbus".equals(info.getName())) {
