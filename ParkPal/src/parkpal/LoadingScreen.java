4d963beeb5f08081d7cc722fabfebc0f1966d828
Drian <Drian@DESKTOP-JBQ6TLJ>
2023-05-07 19:20:14 +0800

    Updated Linkedlist SnakeGame

--- ParkPal/src/parkpal/LoadingScreen.java
+++ ParkPal/src/parkpal/LoadingScreen.java
@@ -231,42 +231,16 @@ public class LoadingScreen extends javax.swing.JFrame {
         // TODO add your handling code here:
     }//GEN-LAST:event_snakeGame2KeyPressed
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String args[]) {
-        /* Set the Nimbus look and feel */
-        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
-        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
-         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
-         */
-        try {
-            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-                if ("Nimbus".equals(info.getName())) {
-                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
-                    break;
-                }
-            }
-        } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(LoadingScreen.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(LoadingScreen.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(LoadingScreen.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(LoadingScreen.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        }
-        //</editor-fold>
-
-        /* Create and display the form */
         LoadingScreen ls = new LoadingScreen();
         ls.setVisible(true);
-        
+
         try {
             for (int i = 0; i <= 100; i++) {
                 Thread.sleep(1000);
-                ls.LoadPercent.setText(i + "%");
                 
+                ls.LoadPercent.setText(i + "%");
+
                 if(i==10){
                     ls.Loading.setText("Turning On Modules...");
                 }
@@ -283,13 +257,13 @@ public class LoadingScreen extends javax.swing.JFrame {
                     ls.Loading.setText("Finishing Up...");
                 }
                 ls.LoadingProgress.setValue(i);
+                System.out.println(i);
             }
         } catch (Exception e) {
             JOptionPane.showMessageDialog(null, e);
         }
-        mpin mpin = new mpin();
-        mpin.setVisible(true);
-        mpin.pack();
+        welcome w = new welcome();
+        w.setVisible(true);
         ls.dispose();
     }
 
--- ParkPal/src/parkpal/welcome.java
+++ ParkPal/src/parkpal/welcome.java
@@ -281,9 +281,8 @@ public class welcome extends javax.swing.JFrame {
         } else if (!Arrays.equals(Pass, Password)){
             JOptionPane.showMessageDialog(null,"Incorrect password. Please try again.", "Incorrect Password", JOptionPane.ERROR_MESSAGE);
         } else if (Arrays.equals(Pass, Password) && User.equals(Username)){
-               LoadingScreen ls = new  LoadingScreen();
-               ls.setVisible(true);
-               ls.pack();
+               mpin m = new  mpin();
+               m.setVisible(true);
                this.dispose();
         }
     }//GEN-LAST:event_LogInButtonActionPerformed
